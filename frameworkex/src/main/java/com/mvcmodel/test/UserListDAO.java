49576e51b22d2eb42bf8c71992ec3aa08fb281ca
dudgh <dudgh@DESKTOP-4QS948Q>
2020-01-19 13:06:07 +0900

    01/19/2020

---
+++ frameworkex/src/main/java/com/mvcmodel/test/UserListDAO.java
@@ -0,0 +1,7 @@
+package com.mvcmodel.test;
+
+import java.util.List;
+
+public interface UserListDAO {
+	public List<UserListResponseDTO> getUserListdao();
+}
---
+++ frameworkex/src/main/java/com/mvcmodel/test/UserListService.java
@@ -0,0 +1,7 @@
+package com.mvcmodel.test;
+
+import java.util.List;
+
+public interface UserListService {
+	public List<UserListResponseDTO> getUserListService();
+}
